c9d213c5bc2cc2425bb29107d7a519c331b22043
plesland <sean@plesland.com>
2026-01-08 21:00:28 -0500

    Create strava_client.py

---
+++ strava_client.py
@@ -0,0 +1,26 @@
+import os
+import requests
+from datetime import datetime, timezone
+
+STRAVA_CLIENT_ID = os.environ["STRAVA_CLIENT_ID"]
+STRAVA_CLIENT_SECRET = os.environ["STRAVA_CLIENT_SECRET"]
+STRAVA_REFRESH_TOKEN = os.environ["STRAVA_REFRESH_TOKEN"]
+
+TOKEN_URL = "https://www.strava.com/oauth/token"
+API_BASE = "https://www.strava.com/api/v3"
+
+
+def get_access_token():
+    resp = requests.post(
+        TOKEN_URL,
+        data={
+            "client_id": STRAVA_CLIENT_ID,
+            "client_secret": STRAVA_CLIENT_SECRET,
+            "refresh_token": STRAVA_REFRESH_TOKEN,
+            "grant_type": "refresh_token",
+        },
+        timeout=10,
+    )
+    resp.raise_for_status()
+    data = resp.json()
+    return data["access_token"]
